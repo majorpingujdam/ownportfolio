a6e5eaf5cf217009bd0202b8e94736d6ae1c6cff
majorpingujdam <151128020+majorpingujdam@users.noreply.github.com>
2026-06-01 16:04:48 -0700

    jnjn

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ if (year) year.textContent = new Date().getFullYear();
 
 // Reveal on scroll
 const revealTargets = document.querySelectorAll(
-  ".intro__image, .intro__text > *, .work__head, .card, .contact__big, .contact__links, .contact-page > *"
+  ".intro__image, .intro__text > *, .work__head, .card, .contact__big, .contact__links, .contact-page > *, .resume__head, .resume__section"
 );
 revealTargets.forEach((el) => el.classList.add("reveal"));
 
